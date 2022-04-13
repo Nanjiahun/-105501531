797e9fbb5da60ab95fb58a688f9beef9fe510951
粘嘉峻 <103197448+Nanjiahun@users.noreply.github.com>
2022-04-13 20:35:38 +0800

    Add files via upload

---
+++ 105501531-0412作業.py
@@ -0,0 +1,20 @@
+
+#功課1:
+for a in range(1,6):
+    for x in range(0,a):
+        print(a,end='')
+    print()
+
+#功課2:
+for a in range(5,0,-1):
+    for x in range(0,a):
+        print(a,end='')
+    print()
+
+#功課3:
+for a in range(9,0,-2):
+    for x in range(0,a):
+        print(a,end='')
+    print()
+
+#功課4:
